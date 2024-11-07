639dfaaa59fa97dbe1d87dd87ef328ead9a0fa91
Brayan Gutierrez Dinarte <38222878+BrayanD17@users.noreply.github.com>
2024-11-06 18:12:04 -0600

    Update App.js

--- proyectosalud/src/App.js
+++ proyectosalud/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import LoginRegisterComponent from './components/login-register/LoginRegisterComponet'; // Importa el componente de login y registro
-import { AuthProvider } from './context/AuthContext'; // Importa AuthProvider
+import LoginRegisterComponent from './components/login-register/LoginRegisterComponet';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <AuthProvider> {/* Envuelve la aplicación en AuthProvider */}
+    <AuthProvider>
       <div className="App">
         <LoginRegisterComponent />
       </div>
