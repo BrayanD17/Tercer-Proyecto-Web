98cb839635f02c7669a62486d571160c9bc4fca7
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2024-11-07 01:36:48 -0600

    Change installs

--- proyectosalud/src/App.js
+++ proyectosalud/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavigationBar from "./components/main-content/NavigationBar";
-import UserProfile from './components/main-content/UserProfile'; // Importa el componente UserProfile
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import LoginRegisterComponent from './components/login-register/LoginRegisterComponet';
 import LoginRegisterComponent from './components/parents/LoginRegisterComponet';
 import { AuthProvider } from './context/AuthContext';
 
