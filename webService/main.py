521952a5017dc022dc983304194cc5ba037d9558
Kevelyn Gonzalez <126025052+KevelynGonzalez@users.noreply.github.com>
2024-11-08 00:43:18 -0600

    KK

--- webService/main.py
+++ webService/main.py
@@ -2,15 +2,19 @@ import io
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime, DECIMAL, ForeignKey, Enum
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, Column, Integer, Float, String, Date, DECIMAL, ForeignKey, Enum,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+from datetime import datetime, timedelta
+
 import csv
-from pydantic import BaseModel, EmailStr, validator, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, EmailStr, validator
 from sqlalchemy.exc import IntegrityError
 from passlib.context import CryptContext
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from typing import Optional
+from fastapi import Path, Query
 from typing import Optional, List
 from fastapi.security import OAuth2PasswordBearer
 
@@ -47,9 +51,13 @@ SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=15))
+    if expires_delta:
+        expire = datetime.datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -64,8 +72,9 @@ class User(Base):
     birthday = Column(DateTime, nullable=False)
     gender = Column(Enum('Masculino', 'Femenino'), nullable=False)
     current_weight = Column(Float, nullable=False)  
-    current_height = Column(Float, nullable=False)
+    current_height = Column(Float, nullable=False)  
 
+    # Relaciones con las otras tablas (las mantenemos igual)
     weights = relationship("Weight", back_populates="user")
     heights = relationship("Height", back_populates="user")
     body_fat_percentages = relationship("BodyFatPercentage", back_populates="user")
@@ -76,28 +85,28 @@ class User(Base):
 
 class Weight(Base):
     __tablename__ = "weights"
-    date = Column(DateTime, primary_key=True, default=datetime.utcnow)
+    date = Column(Date, primary_key=True, default=datetime.today)
     userId = Column(Integer, ForeignKey('users.id'), primary_key=True)
     weight = Column(DECIMAL(5, 2))
     user = relationship("User", back_populates="weights")
 
 class Height(Base):
     __tablename__ = "heights"
-    date = Column(DateTime, primary_key=True, default=datetime.utcnow)
+    date = Column(Date, primary_key=True, default=datetime.today)
     userId = Column(Integer, ForeignKey('users.id'), primary_key=True)
     height = Column(DECIMAL(5, 2))
     user = relationship("User", back_populates="heights")
 
 class BodyFatPercentage(Base):
     __tablename__ = "body_fat_percentages"
-    date = Column(DateTime, primary_key=True, default=datetime.utcnow)  # Usa datetime.utcnow
+    date = Column(Date, primary_key=True, default=datetime.today)
     userId = Column(Integer, ForeignKey('users.id'), primary_key=True)
     fat_percentage = Column(DECIMAL(5, 2))
     user = relationship("User", back_populates="body_fat_percentages")
 
 class BodyComposition(Base):
     __tablename__ = "body_compositions"
-    date = Column(DateTime, primary_key=True, default=datetime.utcnow)  # Usa datetime.utcnow
+    date = Column(Date, primary_key=True, default=datetime.today)
     userId = Column(Integer, ForeignKey('users.id'), primary_key=True)
     fat = Column(DECIMAL(5, 2))
     muscle = Column(DECIMAL(5, 2))
@@ -106,35 +115,37 @@ class BodyComposition(Base):
 
 class WaterConsumption(Base):
     __tablename__ = "water_consumptions"
-    date = Column(DateTime, primary_key=True, default=datetime.utcnow)  # Usa datetime.utcnow
+    date = Column(Date, primary_key=True, default=datetime.today)
     userId = Column(Integer, ForeignKey('users.id'), primary_key=True)
     water_amount = Column(DECIMAL(5, 2))
     user = relationship("User", back_populates="water_consumptions")
 
 class DailySteps(Base):
     __tablename__ = "daily_steps"
-    date = Column(DateTime, primary_key=True, default=datetime.utcnow)  # Usa datetime.utcnow
+    date = Column(Date, primary_key=True, default=datetime.today)
     userId = Column(Integer, ForeignKey('users.id'), primary_key=True)
     steps_amount = Column(Integer)
     user = relationship("User", back_populates="daily_steps")
 
 class Exercise(Base):
     __tablename__ = "exercises"
-    date = Column(DateTime, primary_key=True, default=datetime.utcnow)  # Usa datetime.utcnow
+
+    date = Column(Date, primary_key=True, default=datetime.today)
     userId = Column(Integer, ForeignKey('users.id'), primary_key=True)
     exercise_name = Column(String)
     duration = Column(Integer)
     user = relationship("User", back_populates="exercises")
-    
+
 # Crear las tablas
 Base.metadata.create_all(bind=engine)
+
 
 # Modelo de solicitud
 class RegisterUserRequest(BaseModel):
     email: str
     username: str
     password: str
-    birthday: datetime
+    birthday: datetime.datetime
     gender: str
     current_weight: float
     current_height: float
@@ -146,23 +157,25 @@ class LoginRequest(BaseModel):
 class UserProfileResponse(BaseModel):
     email: str
     username: str
-    birthday: datetime
+    birthday: datetime.datetime
     gender: str
     current_weight: float
     current_height: float
 
-class UserProfileResponse(BaseModel):
-    email: str
-    username: str
-    birthday: datetime
-    gender: str
-    current_weight: float
-    current_height: float
+class UserProfileUpdateRequest(BaseModel):
+    email: str | None = None
+    username: str | None = None
+    birthday: datetime.datetime | None = None
+    gender: str | None = None
+    current_weight: float | None = None
+    current_height: float | None = None
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
 class UserUpdateRequest(BaseModel):
     email: Optional[EmailStr] = None
     username: Optional[str] = None
-    birthday: Optional[datetime] = None 
+    birthday: Optional[datetime.datetime] = None  # Específico como datetime.datetime
     gender: Optional[str] = None
     current_weight: Optional[float] = None
     current_height: Optional[float] = None
@@ -174,7 +187,6 @@ class UserUpdateRequest(BaseModel):
         return value
 
 class PasswordChangeRequest(BaseModel):
-    username: str
     current_password: str
     new_password: str
     
@@ -220,7 +232,7 @@ async def register_user(user: RegisterUserRequest, db: Session = Depends(get_db)
         return {"message": "Usuario registrado con éxito"}
     except IntegrityError as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail="El nombre de usuario ya está registrado")
+        raise HTTPException(status_code=400, detail="El correo electrónico o nombre de usuario ya está registrado")
 
 @app.get("/users/", response_model=List[str])
 async def get_all_usernames(db: Session = Depends(get_db)):
@@ -253,18 +265,12 @@ async def update_user_field(
     update_data: UserUpdateRequest,
     db: Session = Depends(get_db)
 ):
+    # Buscar el usuario en la base de datos
     user = db.query(User).filter(User.username == username).first()
     if not user:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
-    if update_data.birthday:
-        if isinstance(update_data.birthday, str):
-            try:
-                update_data.birthday = datetime.fromisoformat(update_data.birthday)
-            except ValueError:
-                raise HTTPException(status_code=422, detail="Formato de fecha incorrecto, esperado YYYY-MM-DDTHH:MM:SS")
-        user.birthday = update_data.birthday
-
+    # Validar y actualizar cada campo individualmente
     if update_data.email:
         user.email = update_data.email
     if update_data.username:
@@ -272,13 +278,18 @@ async def update_user_field(
         if existing_user:
             raise HTTPException(status_code=400, detail="El nombre de usuario ya está en uso")
         user.username = update_data.username
+    if update_data.birthday:
+        user.birthday = update_data.birthday
     if update_data.gender:
+        if update_data.gender not in ["Masculino", "Femenino"]:
+            raise HTTPException(status_code=400, detail="El género debe ser Masculino o Femenino")
         user.gender = update_data.gender
     if update_data.current_weight is not None:
         user.current_weight = update_data.current_weight
     if update_data.current_height is not None:
         user.current_height = update_data.current_height
 
+    # Guardar cambios en la base de datos
     try:
         db.commit()
         db.refresh(user)
@@ -286,44 +297,35 @@ async def update_user_field(
     except IntegrityError:
         db.rollback()
         raise HTTPException(status_code=400, detail="Error al actualizar el perfil")
-    
-@app.post("/user/change-password")
+
+@app.post("/user/{username}/change-password")
 async def change_password(
+    username: str,
     password_data: PasswordChangeRequest,
     db: Session = Depends(get_db)
 ):
-    print("Iniciando el cambio de contraseña")
-    print("Datos recibidos para cambiar contraseña:", password_data)
-    
-    # Buscar al usuario por el nombre de usuario
-    user = db.query(User).filter(User.username == password_data.username).first()
-    print("Usuario encontrado:", user.username if user else "No encontrado")
-    
-    # Verificar si el usuario existe
+    user = db.query(User).filter(User.username == username).first()
     if not user:
-        print("Usuario no encontrado")
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     
     # Verificar si la contraseña actual es correcta
     if not pwd_context.verify(password_data.current_password, user.password):
-        print("Contraseña actual incorrecta")
         raise HTTPException(status_code=400, detail="La contraseña actual es incorrecta")
     
-    # Validar la nueva contraseña
+    # Validar la nueva contraseña (puedes añadir más validaciones si es necesario)
     if len(password_data.new_password) < 10 or not any(c.isalpha() for c in password_data.new_password) or \
-        not any(c.isdigit() for c in password_data.new_password) or not any(c in '!@#$%^&*(),.?":{}|<>' for c in password_data.new_password):
-        print("Validación de nueva contraseña fallida")
+       not any(c.isdigit() for c in password_data.new_password) or not any(c in '!@#$%^&*(),.?":{}|<>' for c in password_data.new_password):
         raise HTTPException(
             status_code=400, 
             detail="La nueva contraseña debe tener al menos 10 caracteres, incluir letras, números y al menos un símbolo."
         )
-    
+
     # Cifrar la nueva contraseña y actualizarla en la base de datos
     hashed_new_password = pwd_context.hash(password_data.new_password)
     user.password = hashed_new_password
     db.commit()
     db.refresh(user)
-    print("Contraseña actualizada exitosamente")
+
     return {"message": "Contraseña actualizada exitosamente"}
 
 # Servicio para importar datos de sensores
@@ -347,7 +349,7 @@ async def importar_sensores(
         if tipo_dato == "pesos":
             for fila in lector:
                 fecha, peso = fila
-                fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()
+                fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S")
                 peso = float(peso)
                 nuevo_peso = Weight(date=fecha, weight=peso, userId=user_id) 
                 actualizar_o_insertar(db, Weight, nuevo_peso)
@@ -355,7 +357,7 @@ async def importar_sensores(
         elif tipo_dato == "alturas":
             for fila in lector:
                 fecha, altura = fila
-                fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()
+                fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S")
                 altura = float(altura)
                 nueva_altura = Height(date=fecha, height=altura, userId=user_id)
                 actualizar_o_insertar(db, Height, nueva_altura)
@@ -363,7 +365,7 @@ async def importar_sensores(
         elif tipo_dato == "composicion_corporal":
             for fila in lector:
                 fecha, grasa, musculo, agua = fila
-                fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()
+                fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()
                 grasa = float(grasa)
                 musculo = float(musculo)
                 agua = float(agua)
@@ -373,14 +375,14 @@ async def importar_sensores(
         elif tipo_dato == "porcentaje_grasa":
             for fila in lector:
                 fecha, porcentaje_grasa = fila
-                fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()
+                fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S")
                 porcentaje_grasa = float(porcentaje_grasa)
                 nuevo_porcentaje_grasa = BodyFatPercentage(date=fecha, fat_percentage=porcentaje_grasa,userId=user_id)
                 actualizar_o_insertar(db, BodyFatPercentage, nuevo_porcentaje_grasa)
         elif tipo_dato == "vasos_de_agua":
             for fila in lector:
                 fecha, vasos_agua = fila
-                fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()  # Usar la fecha del archivo
+                fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S") # Usar la fecha del archivo
                 vasos_agua = int(vasos_agua)
                 nuevo_consumo_agua = WaterConsumption(date=fecha, water_amount=vasos_agua, userId=user_id)
                 actualizar_o_insertar(db, WaterConsumption, nuevo_consumo_agua)
@@ -388,7 +390,7 @@ async def importar_sensores(
         elif tipo_dato == "pasos_diarios":
             for fila in lector:
                 fecha, cantidad_pasos = fila
-                fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()  # Usar la fecha del archivo
+                fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S")  # Usar la fecha del archivo
                 cantidad_pasos = int(cantidad_pasos)
                 nuevos_pasos = DailySteps(date=fecha, steps_amount=cantidad_pasos, userId=user_id)
                 actualizar_o_insertar(db, DailySteps, nuevos_pasos)
@@ -396,7 +398,7 @@ async def importar_sensores(
         elif tipo_dato == "ejercicios":
             for fila in lector:
                 fecha, nombre_ejercicio, duracion = fila
-                fecha = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S").date()
+                fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S")
                 duracion = int(duracion)
                 nuevo_ejercicio = Exercise(date=fecha, exercise_name=nombre_ejercicio, duration=duracion, userId=user_id)
                 actualizar_o_insertar(db, Exercise, nuevo_ejercicio)
@@ -411,6 +413,7 @@ async def importar_sensores(
         db.rollback()
         raise HTTPException(status_code=400, detail=f"Error al importar datos: {e}")
 
+
 # Configuración del token
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
@@ -419,9 +422,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
