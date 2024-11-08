576de6f2fbc502d13969bfa29a806a3e016f8530
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2024-11-07 22:16:00 -0600

    Enpoint getDataDashboard

--- proyectosalud/src/context/AuthContext.js
+++ proyectosalud/src/context/AuthContext.js
@@ -5,6 +5,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [username, setUsername] = useState(null);
+  const [dashboardData,setDashboardData]=useState(null);
 
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
@@ -170,17 +171,45 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const getDataDashboardOverview = async()=>{
+    if(!token){
+      console.error("Token no disponible");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/dashboard_overview/",{
+        method:"GET",
+        headers:{
+           Authorization: `Bearer ${token}`,
+        }
+      });
+
+      if(response.ok){
+        const data = await response.json();
+        console.log("Datos obtenidos del servidor:", data);  // Agrega este log
+        setDashboardData(data);
+      }else{
+        throw new Error("Error al obtener los datos los datos del usuario para mostrarlos en el dashboard")
+      }
+    } catch (error) {
+      console.error("Error al obtener los datos desde el server", error)
+    }
+  }
+
   return (
     <AuthContext.Provider value={{ 
       token, 
       username,
+      dashboardData,
       login, 
       register, 
       getUsernames, 
       getUserProfile,  
       updateUserField,
       changePassword, 
-      logout :logoutUser
+      logout :logoutUser,
+      getDataDashboardOverview,
     }}>
       {children}
     </AuthContext.Provider>
